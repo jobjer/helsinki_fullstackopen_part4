7041cd45e0e0960df12837a4578ef4c1a9ae78a0
Jostein Bjerga <jobjer@gmail.com>
2023-01-30 15:15:31 +0100

    Solution Exercise 4.6 and 4.7

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -20,7 +20,44 @@ const favoriteBlog = (blogs) => {
   }
 }
 
+const authorWithMostBlogs = (blogs) => {
+  
+  const blogsPerAuthor = []
+
+  blogs.forEach(blog => {
+    let authorAdded = {}
+    authorAdded = blogsPerAuthor.find(({author}) => author === blog.author)
+
+    if(authorAdded === undefined){
+      let blogToAdd = { author: blog.author, blogs: 1 }
+      blogsPerAuthor.push(blogToAdd)
+    } else {
+      let objIndex = blogsPerAuthor.findIndex((obj => obj.author === blog.author))
+      blogsPerAuthor[objIndex].blogs = blogsPerAuthor[objIndex].blogs + 1
+    }
+  })
+  return blogsPerAuthor.reduce((prev, current) => (prev.blogs > current.blogs) ? prev : current)
+}
+
+const authorWithMostLikes = (blogs) => {
+  const likesPerAuthor = []
+
+  blogs.forEach(blog => {
+    let authorAdded = {}
+    authorAdded = likesPerAuthor.find(({author}) => author === blog.author)
+
+    if(authorAdded === undefined){
+      let blogToAdd = { author: blog.author, likes: blog.likes }
+      likesPerAuthor.push(blogToAdd)
+    } else {
+      let objIndex = likesPerAuthor.findIndex((obj => obj.author === blog.author))
+      likesPerAuthor[objIndex].likes = likesPerAuthor[objIndex].likes + blog.likes
+    }
+  })
+  return likesPerAuthor.reduce((prev, current) => (prev.likes > current.likes) ? prev : current)
+
+}
 
 module.exports = {
-  dummy, totalLikes, favoriteBlog
+  dummy, totalLikes, favoriteBlog, authorWithMostBlogs, authorWithMostLikes
 }
