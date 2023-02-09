eb12f6cf46d6b1e410552f168fdb47baac908319
Jostein Bjerga <jobjer@gmail.com>
2023-02-09 14:24:45 +0100

    Solution Exercise 4.20

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -3,13 +3,7 @@ const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
 
-const getTokenFrom = request => {
-  const authorization = request.get('authorization')
-  if (authorization && authorization.startsWith('Bearer ')) {
-    return authorization.replace('Bearer ', '')
-  }
-  return null
-}
+
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog
@@ -46,7 +40,7 @@ blogsRouter.post('/', async (request, response, next) => {
     }
 
     const body = request.body
-    const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+    const decodedToken = jwt.verify(request.token, process.env.SECRET)
     if (!decodedToken.id) {
       return response.status(401).json({ error: 'token invalid' })
     }
